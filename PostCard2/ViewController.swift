ca0ef230891109a9bb48d61b9b4e941ade9d8d71
storm0070 <unpackjarfile@gmail.com>
2014-10-13 19:26:34 +0900

    First Commit

--- PostCard2/ViewController.swift
+++ PostCard2/ViewController.swift
@@ -10,6 +10,13 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var messageLabel: UILabel!
+    @IBOutlet weak var enterNameTextField: UITextField!
+    @IBOutlet weak var enterMessageField: UITextField!
+    @IBOutlet weak var mailButton: UIButton!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +27,18 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func sendMailButtonPressed(sender: UIButton) {
+        // 이거 커밋되나 ?ㅋㅋㅋㅋzzz
+        messageLabel.hidden = false
+        messageLabel.text = enterMessageField.text
+        messageLabel.textColor = UIColor.redColor()
+        enterMessageField.text=""
+        enterMessageField.resignFirstResponder()
+        mailButton.setTitle("메일전송 완료", forState: UIControlState.Normal)
+        
+    }
 
+  
+     
 }
 
